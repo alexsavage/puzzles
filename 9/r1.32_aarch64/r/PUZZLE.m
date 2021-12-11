dd58e14596d015edf7d3466941214f2ae0a03cdf
Alex Savage <alexsavage@users.noreply.github.com>
2021-12-10 21:37:55 -0600

    use more commonly-understood 'X' and 'Y'

--- 9/r1.32_aarch64/r/PUZZLE.m
+++ 9/r1.32_aarch64/r/PUZZLE.m
@@ -1,8 +1,8 @@
 PUZZLE
-	n X,Y,Map,Value,NeighborX,NeighborY,NeighborVal,Risk
+	n X,Y,Map,Value,Risk
 	s Risk=0
 	w !,"Input (100x100):",!
-	f X=1:1:100 r Line f Y=1:1:100 s Map(X,Y)=$e(Line,Y)
+	f Y=1:1:100 r Line f X=1:1:100 s Map(X,Y)=$e(Line,X)
 	f X=1:1:100 f Y=1:1:100 DO
 	. s Value=Map(X,Y)
 	. q:'$$Compare(.Map,Value,X-1,Y)
