db0a934ce4483db86c76a7c1afec0fce54518b06
Alex Savage <alexsavage@users.noreply.github.com>
2022-12-03 00:21:44 -0600

    part 2

--- advent-of-code-2022/dec03/src/main.rs
+++ advent-of-code-2022/dec03/src/main.rs
@@ -2,22 +2,22 @@ use std::io::stdin;
 
 fn priority(item: char) -> usize {
     match item {
-        'a'..='z' => item as usize - 96,
-        'A'..='Z' => item as usize - 64 + 26,
+        'a'..='z' => item as usize - 96,      // 1..26
+        'A'..='Z' => item as usize - 64 + 26, // 27..52
         _ => panic!(),
     }
 }
 
-fn find_overlap(line: &str) -> char {
+fn find_overlap_1(line: &str) -> char {
     let mut seen: [bool; 52] = [false; 52];
     let chars = line.chars();
     let count = chars.count();
 
     for (index, item) in line.char_indices() {
-        if index < count/2 {
-            seen[priority(item)-1] = true;
+        if index < count / 2 {
+            seen[priority(item) - 1] = true;
         } else {
-            if seen[priority(item)-1] {
+            if seen[priority(item) - 1] {
                 return item;
             }
         }
@@ -25,11 +25,64 @@ fn find_overlap(line: &str) -> char {
     panic!("No overlap found.")
 }
 
-fn main() {
-    let mut sum: usize = 0;
-    for line in stdin().lines().flatten() {
-        sum += priority(find_overlap(line.as_str()));
+fn find_overlap_2(line1: &str, line2: &str, line3: &str) -> char {
+    let mut seen: [u8; 52] = [0; 52];
+
+    for item in line1.chars() {
+        let index = priority(item) - 1;
+        if seen[index] == 0 {
+            seen[index] = 1;
+        }
     }
 
-    println!("Part 1: {}", sum);
+    for item in line2.chars() {
+        let index = priority(item) - 1;
+        if seen[index] == 1 {
+            seen[index] = 2;
+        }
+    }
+
+    for item in line3.chars() {
+        let index = priority(item) - 1;
+        if seen[index] == 2 {
+            return item;
+        }
+    }
+
+    panic!("No overlap found.")
+}
+
+enum State {
+    One,
+    Two,
+    Three,
+}
+fn main() {
+    let mut sum_1: usize = 0;
+    let mut sum_2: usize = 0;
+    let mut state: State = State::One;
+    let mut line_1: String = Default::default();
+    let mut line_2: String = Default::default();
+
+    for line in stdin().lines().flatten() {
+        sum_1 += priority(find_overlap_1(&line));
+        match state {
+            State::One => {
+                line_1 = line;
+                state = State::Two;
+            }
+            State::Two => {
+                line_2 = line;
+                state = State::Three;
+            }
+            State::Three => {
+                sum_2 += priority(find_overlap_2(&line_1, &line_2, &line));
+                state = State::One;
+            }
+        }
+    }
+
+    println!();
+    println!("Part 1: {}", sum_1);
+    println!("Part 2: {}", sum_2);
 }
