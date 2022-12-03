48bb29065872e2f76bdd704120cee5d2b3870150
Alex Savage <alexsavage@users.noreply.github.com>
2022-12-02 23:55:10 -0600

    part 1

--- advent-of-code-2022/dec03/src/main.rs
+++ advent-of-code-2022/dec03/src/main.rs
@@ -1,7 +1,35 @@
 use std::io::stdin;
 
-fn main() {
-    for line in stdin().lines().flatten() {
-        println!("{}", line)
+fn priority(item: char) -> usize {
+    match item {
+        'a'..='z' => item as usize - 96,
+        'A'..='Z' => item as usize - 64 + 26,
+        _ => panic!(),
     }
+}
+
+fn find_overlap(line: &str) -> char {
+    let mut seen: [bool; 52] = [false; 52];
+    let chars = line.chars();
+    let count = chars.count();
+
+    for (index, item) in line.char_indices() {
+        if index < count/2 {
+            seen[priority(item)-1] = true;
+        } else {
+            if seen[priority(item)-1] {
+                return item;
+            }
+        }
+    }
+    panic!("No overlap found.")
+}
+
+fn main() {
+    let mut sum: usize = 0;
+    for line in stdin().lines().flatten() {
+        sum += priority(find_overlap(line.as_str()));
+    }
+
+    println!("Part 1: {}", sum);
 }
